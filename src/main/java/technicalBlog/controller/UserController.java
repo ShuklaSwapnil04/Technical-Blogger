7819b394ef3e46a116f0338f17d9382a3bd26e9c
Swapnil <shuklaswapnil001@gmail.com>
2020-02-27 20:29:32 +0530

    sixth commit

--- src/main/java/technicalBlog/controller/UserController.java
+++ src/main/java/technicalBlog/controller/UserController.java
@@ -1,12 +1,21 @@
 package technicalBlog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import technicalBlog.model.Post;
 import technicalBlog.model.User;
+import technicalBlog.service.PostService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
     @RequestMapping("users/login")
   public  String login()
   {
@@ -22,4 +31,13 @@ public class UserController {
   {
       return "redirect:/posts";
   }
+
+    @RequestMapping(value="users/logout", method = RequestMethod.POST)
+  public String logout(Model model)
+    {
+        ArrayList<Post> posts=postService.getAllPosts();
+        model.addAttribute("posts",posts);
+        return "index";
+
+    }
 }
