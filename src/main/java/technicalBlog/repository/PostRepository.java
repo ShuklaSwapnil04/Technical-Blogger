1491349f42c0b8dbcafd56b5507c177be929f6a2
Swapnil <shuklaswapnil001@gmail.com>
2020-03-30 22:34:00 +0530

    final commit

--- src/main/java/technicalBlog/repository/PostRepository.java
+++ src/main/java/technicalBlog/repository/PostRepository.java
@@ -1,4 +1,27 @@
 package technicalBlog.repository;
 
+import technicalBlog.model.Post;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class PostRepository {
+    @PersistenceUnit(unitName = "techblog")
+    private EntityManagerFactory emf;
+
+    public List<Post> getAllPosts() {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Post> query = em.createQuery("SELECT p from Post p", Post.class);
+        List<Post> resultList = query.getResultList();
+
+        return resultList;
+    }
+
+    public Post getLatestPost() {
+        EntityManager em = emf.createEntityManager();
+        return em.find(Post.class, 3);
+    }
 }
